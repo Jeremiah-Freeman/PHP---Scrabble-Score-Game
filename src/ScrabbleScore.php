84f7c96df8d1331d7e27249267a0b375ea27eb8a
Ash Laidlaw and Jay Freeman <jay@>
2017-02-16 10:10:23 -0800

    Add second test for other letters, test passes

--- src/ScrabbleScore.php
+++ src/ScrabbleScore.php
@@ -6,10 +6,18 @@
             $input = array('k' => 5);
             return $input['k'];
         }
+
+        function test_Whole_Word($input)
+        {
+            $input = array('l' => 1,'o' => 1,'v' => 4,'e' => 1);
+            $output = 0;
+
+            foreach($input as $value)
+            {
+                $output += $value;
+            }
+
+            return $output;
+        }
     }
-
-
-
-
-
 ?>
--- tests/ScrabbleScoreTest.php
+++ tests/ScrabbleScoreTest.php
@@ -14,7 +14,20 @@
             $result = $test_ScrabbleScore->test_Add_Singles($input);
 
             //Assert
-            $this->assertEquals("5" , $result);
+            $this->assertEquals(5, $result);
+        }
+
+        function test_Whole_Word()
+        {
+            //Arrange
+            $test_ScrabbleScore = new ScrabbleScore;
+            $input = "love";
+
+            //Act
+            $result = $test_ScrabbleScore->test_Whole_Word($input);
+
+            //Assert
+            $this->assertEquals(7 , $result);
         }
     }
 
